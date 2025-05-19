8bf133cacdf5504feaaad58e31eb3978c381e548
Rangdog <thanhhaxuan02@gmail.com>
2025-05-19 22:00:01 +0700

    first commit

---
+++ src/test/java/com/example/quartz_demo_duplicate/QuartzDemoDuplicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartz_demo_duplicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzDemoDuplicateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
